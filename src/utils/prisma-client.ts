3679056203b5a111c7550653fc076c8e95a518f4
Mohamedelbayoumi <mohamedalbyoumi822@gmail.com>
2024-09-16 21:35:07 +0300

    database url

--- src/utils/prisma-client.ts
+++ src/utils/prisma-client.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({ log: ['query'] })
+console.log(process.env.DATABASE_URL)
+
+export const prisma = new PrismaClient({ log: ['query'], datasources: { db: { url: process.env.DATABASE_URL } } })
