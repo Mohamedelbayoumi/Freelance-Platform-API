752c3e9b7b3eaef9fba72b66fe085b7e09e9a561
mohamedelbayoumi <mohamedalbayoumi822@gmail.com>
2024-09-18 21:42:43 +0300

    adding update and delete offer

--- src/services/offer.services.ts
+++ src/services/offer.services.ts
@@ -1,10 +1,18 @@
+import { Prisma } from '@prisma/client'
+
 import { prisma } from '../utils/prisma-client'
 
 export class OfferService {
-    public static async findAll(taskId: number) {
-        const offers = await prisma.offer.findMany({
+
+    private offerModel = prisma.offer
+
+    public async findAll(taskId: number, freelancerId: number) {
+        const offers = await this.offerModel.findMany({
             where: {
-                task_id: taskId
+                task_id: taskId,
+                freelancer_id: {
+                    not: freelancerId
+                }
             },
             select: {
                 freelancer: {
@@ -16,23 +24,71 @@ export class OfferService {
                         average_rating: true
                     }
                 },
+                id: true,
                 description: true
             }
         })
-        return offers
+        let offer = await this.findLoggedInFreelancerOffer(taskId, freelancerId)
+
+        if (offer) {
+            offer['userLoggedin'] = true
+        }
+
+        return [offer, offers]
     }
 
-    public static async create(
-        implementationDuration: number, askingPrice: number, description: string,
-        taskId: number, freelancerId: number
+    private async findLoggedInFreelancerOffer(taskId: number, freelancerId: number) {
+        return await this.offerModel.findUnique({
+            where: {
+                task_id_freelancer_id: {
+                    task_id: taskId,
+                    freelancer_id: freelancerId
+                }
+            }
+        })
+
+    }
+
+    public async create(
+        implementationDuration: number, askingPrice: number, taskId: number,
+        description: string, freelancerId: number
     ) {
-        await prisma.offer.create({
+        await this.offerModel.create({
             data: {
                 description: description,
                 asking_price: askingPrice,
                 implementation_duration: implementationDuration,
                 task_id: taskId,
                 freelancer_id: freelancerId
+            }
+        })
+    }
+
+    public async update(
+        offerId: number, askingPrice?: number, implementationDuration?: number,
+        description?: string
+    ) {
+
+        const data: Prisma.OfferUpdateInput = {
+            asking_price: askingPrice
+        }
+
+        await this.offerModel.update({
+            data: {
+                asking_price: askingPrice,
+                implementation_duration: implementationDuration,
+                description: description,
+            },
+            where: {
+                id: offerId
+            }
+        })
+    }
+
+    public async delete(offerId: number) {
+        return await this.offerModel.delete({
+            where: {
+                id: offerId
             }
         })
     }
